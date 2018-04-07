faa365b4e59059a03c8f4af0d375b825840b2f05
Isaiah Gaspar <igaspar@asu.edu>
2018-04-07 13:47:55 -0700

    Add files via upload

---
+++ fws_cam/fws_cam.ino
@@ -0,0 +1,43 @@
+
+// © Adafruit Industries https://learn.adafruit.com/timelapse-spy-camera
+ /*
+  * Edited by Isaiah Gaspar
+  * 
+  * Date: 3/31/18
+  * 
+  */
+
+//IG Changed pins to work with teensy 3.2
+int trig = 23;
+int led = 13;
+
+void setup() {
+  
+  // initialize the digital pins as output.
+  pinMode(led, OUTPUT);
+  
+  pinMode(trig, OUTPUT);
+  
+  digitalWrite(led, HIGH);
+  
+  digitalWrite(trig, HIGH);
+}
+
+// Hold HIGH and trigger quick (<250ms) LOW to take a photo. Holding LOW and trigger HIGH starts/stops video recording
+void loop() {
+  
+  digitalWrite(trig, LOW);
+  digitalWrite(led, LOW);
+  
+//IG Changed time
+  delay(50);
+  
+  digitalWrite(trig, HIGH);
+  digitalWrite(led, HIGH);
+  
+  //Delay between pictures
+  
+//IG Changed time  
+  delay(500);
+}
+
